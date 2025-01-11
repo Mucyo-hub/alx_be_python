90b9684c0f3a13e98f0443ced74ac086c6bd2d55
Mucyo Fabrice <mucyofabrice26@gmail.com>
2025-01-11 15:32:11 +0200

    Add shopping list manager script for managing a shopping list

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -9,35 +9,39 @@ def main():
     shopping_list = []
     while True:
         display_menu()
-        choice = int(input("Enter your choice: "))
+        try:
+            choice = int(input("Enter your choice: "))
 
-        if choice == '1':
-            item = input("Enter the item name: ")
-            shopping_list.append(item)
-            print(f"Added {item} to the list")
-            
-        elif choice == '2':
-            item = input("Enter item to remove: ")
-            if item in shopping_list:
-                shopping_list.remove(item)
-                print(f"Removed {item} from the list")
+            if choice == 1:
+                item = input("Enter the item name: ")
+                shopping_list.append(item)
+                print(f"Added {item} to the list")
+                
+            elif choice == 2:
+                item = input("Enter item to remove: ")
+                if item in shopping_list:
+                    shopping_list.remove(item)
+                    print(f"Removed {item} from the list")
+                else:
+                    print("Item not found in the list")
+                
+            elif choice == 3:
+                if shopping_list:
+                    print("\nShopping List:")
+                    for item in shopping_list:
+                        print(f"- {item}")
+                else:
+                    print("Shopping list is empty")
+                
+            elif choice == 4:
+                print("Goodbye!")
+                break
+                
             else:
-                print("Item not found in the list")
-            
-        elif choice == '3':
-            if shopping_list:
-                print("\nShopping List:")
-                for item in shopping_list:
-                    print(f"- {item}")
-            else:
-                print("Shopping list is empty")
-            
-        elif choice == '4':
-            print("Goodbye!")
-            break
-            
-        else:
-            print("Invalid choice. Please try again.")
+                print("Invalid choice. Please try again.")
+                
+        except ValueError:
+            print("Invalid input. Please enter a number.")
 
 if __name__ == "__main__":
     main()
